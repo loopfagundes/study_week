5c27bdeeb5995a0edbb32f182b286f9c2aa48c3b
Luciano Fagundes <luciano.fagundes@dbserver.com.br>
2021-03-02 18:00:26 -0300

    update BaseTest

--- src/main/java/br/com/dbserver/utils/BaseTest.java
+++ src/main/java/br/com/dbserver/utils/BaseTest.java
@@ -1,28 +1,26 @@
 package br.com.dbserver.utils;
 
+import br.com.dbserver.webdrivers.BrowserEnum;
+import br.com.dbserver.webdrivers.DriverFactory;
+import br.com.dbserver.webdrivers.DriverManager;
 import com.aventstack.extentreports.testng.listener.ExtentITestListenerClassAdapter;
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Listeners;
 
 @Listeners({ExtentITestListenerClassAdapter.class})
 public class BaseTest {
-    protected WebDriver driver;
 
     @BeforeMethod
     public void setUp() {
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+        WebDriver driver = DriverFactory.createInstance(BrowserEnum.CHROME);
+        DriverManager.setDriver(driver);
         driver.manage().window().maximize();
     }
 
     @AfterMethod
     public void tearDown() {
-        if (driver != null) {
-            driver.quit();
-        }
+        DriverManager.closeDriver();
     }
 }
